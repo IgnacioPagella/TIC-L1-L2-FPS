dd87ea57766e38e6f7e5172272dde9e4c00e008f
IgnacioPagella <nacho.pagella@gmail.com>
2022-08-05 10:30:19 -0300

    update

---
+++ Assets/Scripts/CarroNets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroNets : MonoBehaviour
+{
+    public GameObject net;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (Input.GetKey(KeyCode.N) && col.gameObject.tag == "netbook")
+        {
+            Instantiate(net);
+        }
+    }
+}
